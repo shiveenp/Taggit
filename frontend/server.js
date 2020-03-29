a52bda77c2d9443ad0c04f79c68d8cab8538dd1a
shiveenpandita <shiveenpandita@gmail.com>
2020-03-29 17:30:23 +1100

    chore: move server.js to root

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const serveStatic = require("serve-static");
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
