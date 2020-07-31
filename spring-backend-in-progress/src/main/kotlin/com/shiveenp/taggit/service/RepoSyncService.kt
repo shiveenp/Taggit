f6ee50cc2b25b6920d35d50de61fec3a08008715
shiveenpandita <shiveenpandita@gmail.com>
2020-07-31 21:20:26 +1000

    Added docker compose for db and redis

--- spring-backend-in-progress/src/main/kotlin/com/shiveenp/taggit/service/RepoSyncService.kt
+++ spring-backend-in-progress/src/main/kotlin/com/shiveenp/taggit/service/RepoSyncService.kt
@@ -1,24 +1,15 @@
 package com.shiveenp.taggit.service
 
 import com.shiveenp.taggit.models.GithubStargazingResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.withContext
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
-import org.springframework.web.server.ServerWebExchange
 
 /**
  * This is the main service responsible for all the nitty gritty of repo sync logic that
@@ -32,8 +23,8 @@ class RepoSyncService(val githubService: GithubService,
 
     suspend fun syncUserStargazingData(): String {
         ReactiveSecurityContextHolder.getContext().doOnNext {
-            val oauthtoken = it.authentication as OAuth2AuthenticationToken
-            clientService.loadAuthorizedClient<OAuth2AuthorizedClient>(oauthtoken.authorizedClientRegistrationId, oauthtoken.name).doOnSuccess {
+            val authToken = it.authentication as OAuth2AuthenticationToken
+            clientService.loadAuthorizedClient<OAuth2AuthorizedClient>(authToken.authorizedClientRegistrationId, authToken.name).doOnSuccess {
                 logger.info { "Token is: ${it.accessToken.tokenValue}" }
             }.subscribe()
             val startPage = 1
@@ -69,6 +60,5 @@ class RepoSyncService(val githubService: GithubService,
         } else {
             null
         }
-
     }
 }
