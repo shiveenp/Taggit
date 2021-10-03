7083b8294ac460b1ae5340c6daa344e5e7e3db95
shiveenpandita <shiveenpandita@gmail.com>
2021-10-03 14:41:59 +1100

    Added test module to grab new jwt keys and changed the prod url for frontend to point to the api.taggit.app domain

--- backend/src/main/kotlin/com/shiveenp/taggit/security/EncryptorService.kt
+++ backend/src/main/kotlin/com/shiveenp/taggit/security/EncryptorService.kt
@@ -4,7 +4,6 @@ import org.springframework.security.crypto.encrypt.Encryptors
 import org.springframework.security.crypto.keygen.KeyGenerators
 import org.springframework.stereotype.Service
 
-
 @Service
 class EncryptorService: Encryptor {
     override fun encrypt(plainMessage: String, cipherKey: String): String {
